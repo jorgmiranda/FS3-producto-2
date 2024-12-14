5f9d142f625bfd751ffddfe9bb9236ecad810906
jorgmiranda <163704881+jorgmiranda@users.noreply.github.com>
2024-12-14 00:32:31 -0300

    Update ProductoRepository.java

--- src/main/java/com/fs3/repository/ProductoRepository.java
+++ src/main/java/com/fs3/repository/ProductoRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.fs3.model.Producto;
 
 public interface ProductoRepository extends JpaRepository<Producto, Long>{
-    List<Producto> findByTipoProducto(String tipoProducto);
+    List<Producto> findByTipoProducto(String tipoProducto); 
 }
